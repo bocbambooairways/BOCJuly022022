23e5313bfbbf3b48aaa21a8dd2415fec5006118f
bocbambooairways <bocbambooairways@gmail.com>
2022-07-02 09:56:28 +0700

    BOC July 02072022

---
+++ BOC/Areas/B-DCS/Models/DCS_SeatMap_Get_One_Pax.cs
@@ -0,0 +1,11 @@
+﻿namespace BOC.Areas.B_DCS.Models
+{
+    public class DCS_SeatMap_Get_One_Pax
+    {
+        public string col { get; set; }
+        public string SeatNo { get; set; }
+        public string Attr { get; set; }
+        public string PNR { get; set; }
+
+    }
+}
